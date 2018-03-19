1ae5241bfb7803be484a5867b39fd6b35eda7568
AlexanderJChoi <alexanderajchoi@gmail.com>
2018-03-19 11:23:01 -0400

    Implement basic structure of Lore database
    (edit: identified correct author)

---
+++ Entry.h
@@ -0,0 +1,21 @@
+#ifndef __ENTRY_H__
+#define __ENTRY_H__
+
+#include <string>
+#include <vector>
+
+class Entry {
+  private:
+    std::string title, content;
+    std::vector<std::string> assoc_pages;
+  public:
+    Entry(const std::string& t, const std::string& c, std::vector<std::string>& p) : title(t), content(c), assoc_pages() {
+      this->assoc_pages.swap(p);
+    }
+
+    const std::string& getTitle() { return this->title; };
+    const std::string& getContent() { return this->content; };
+    const std::vector<std::string>& getAssociatedLorePages() { return this->assoc_pages; };
+};
+
+#endif
---
+++ LorePage.h
@@ -0,0 +1,42 @@
+#ifndef __LORE_PAGE_H__
+#define __LORE_PAGE_H__
+
+#include <string>
+#include <vector>
+
+#include "./Entry.h"
+
+class LorePage {
+  private:
+    std::string title;
+    std::vector<Entry* > entries;
+  public:
+    LorePage(std::string t) : title(t) {} ;
+    ~LorePage() {
+      for(Entry* e : this->entries) {
+        delete e;
+      }
+    };
+
+    void addEntry(Entry* e) {
+      for(Entry* all : this->entries) {
+        if(all->getTitle() == e->getTitle()) return;
+      }
+      this->entries.push_back(e);
+    };
+
+    void removeEntry(std::string entryTitle) {
+      for(auto it = this->entries.begin(); it != this->entries.end(); it++) {
+        if((*it)->getTitle() == entryTitle) {
+          delete (*it);
+          this->entries.erase(it);
+          break;
+        }
+      }
+    }
+
+    const std::string& getTitle() { return this->title; };
+    const std::vector<Entry* >& getEntries() { return this->entries; };
+};
+
+#endif
